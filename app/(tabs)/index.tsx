e4c72a2255602e7e685cdb92c111ac4bcc22108d
unknown <flourishitunu001@gmail.com>
2025-08-25 18:26:11 +0100

    Adding the todo lists

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,32 @@
 import { createHomeStyles } from "@/assets/styles/home.styles";
 import Header from "@/components/Header";
+import LoadingSpinner from "@/components/LoadingSpinner";
 import TodoInput from "@/components/TodoInput";
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/UseTheme";
+import { useQuery } from "convex/react";
 import { LinearGradient } from "expo-linear-gradient";
-import { StatusBar, Text, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import { FlatList, StatusBar, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const { toggleDarkMode, colors } = useTheme();
   const HomeStyles = createHomeStyles(colors);
+
+  const todos = useQuery(api.todos.getTodos);
+  const isLoading=todos===undefined
+  if (isLoading) return <LoadingSpinner/>
   return (
     <LinearGradient
       colors={colors.gradients.background}
       style={HomeStyles.container}
     >
-      <StatusBar barStyle={colors.statusBarStyle}/>
+      <StatusBar barStyle={colors.statusBarStyle} />
       <SafeAreaView style={HomeStyles.safeArea}>
-        <Header/>
-        <TodoInput/>
-        <TouchableOpacity onPress={toggleDarkMode}>
-          <Text>hello</Text>
-        </TouchableOpacity>
+        <Header />
+        <TodoInput />
+        <FlatList data={todos} renderItem={renderTodoItem} keyExtractor={(item)=>item._id} style={HomeStyles.todoList}/>
       </SafeAreaView>
     </LinearGradient>
   );
