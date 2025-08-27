4e8462cf164475f32453021b4a784e638016a02a
unknown <flourishitunu001@gmail.com>
2025-08-27 17:45:28 +0100

    make a change

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,20 +3,25 @@ import Header from "@/components/Header";
 import LoadingSpinner from "@/components/LoadingSpinner";
 import TodoInput from "@/components/TodoInput";
 import { api } from "@/convex/_generated/api";
+import { Doc } from "@/convex/_generated/dataModel";
 import useTheme from "@/hooks/UseTheme";
 import { useQuery } from "convex/react";
 import { LinearGradient } from "expo-linear-gradient";
-import { useState } from "react";
-import { FlatList, StatusBar, Text } from "react-native";
+import { FlatList, StatusBar } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+type Todo = Doc<"todos">;
 
 export default function Index() {
   const { toggleDarkMode, colors } = useTheme();
   const HomeStyles = createHomeStyles(colors);
 
   const todos = useQuery(api.todos.getTodos);
-  const isLoading=todos===undefined
-  if (isLoading) return <LoadingSpinner/>
+  const isLoading = todos === undefined;
+  if (isLoading) return <LoadingSpinner />;
+  const renderTodoItem = (item: { item: Todo }) => {
+    return;
+  };
   return (
     <LinearGradient
       colors={colors.gradients.background}
@@ -26,7 +31,13 @@ export default function Index() {
       <SafeAreaView style={HomeStyles.safeArea}>
         <Header />
         <TodoInput />
-        <FlatList data={todos} renderItem={renderTodoItem} keyExtractor={(item)=>item._id} style={HomeStyles.todoList}/>
+        {/* <FlatList
+          data={todos}
+          // renderItem={renderTodoItem}
+          keyExtractor={(item) => item._id}
+          style={HomeStyles.todoList}
+          contentContainerStyle={HomeStyles.todoListContent}
+        /> */}
       </SafeAreaView>
     </LinearGradient>
   );
